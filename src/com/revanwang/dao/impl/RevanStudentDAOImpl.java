93624bc1b03fed7dde855b74095ed1af4034fcda
RevanWang <zjqx1991@163.com>
2019-07-02 20:59:21 +0800

    DAO测试

--- src/com/revanwang/dao/impl/RevanStudentDAOImpl.java
+++ src/com/revanwang/dao/impl/RevanStudentDAOImpl.java
@@ -3,6 +3,11 @@
  */
 package com.revanwang.dao.impl;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revanwang.dao.IRevanStudentDAO;
@@ -18,27 +23,263 @@ public class RevanStudentDAOImpl implements IRevanStudentDAO {
 
 	@Override
 	public void sava(RevanStudent student) {
+		String sql = "INSERT INTO t_student (name, age) VALUES (?, ?)";
+		//贾琏欲执事
+		//获取连接对象
+		Connection connection = null;
+		//SQL语句对象
+		PreparedStatement ps = null;
 		
+		try {
+			//1、加载驱动
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//2、获取连接对象
+			connection = DriverManager.getConnection("jdbc:mysql:///jdbcdemo", "root", "admin");
+			//3、获取执行SQL语句对象
+			ps = connection.prepareStatement(sql);
+			ps.setString(1, student.getName());
+			ps.setInt(2, student.getAge());
+			//4、执行SQL
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (connection != null) {
+						connection.close();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	
 	}
 
 	@Override
 	public void delete(Long id) {
+		String sql = "DELETE FROM t_student WHERE id = ?";
+		//贾琏欲执事
+		//获取连接对象
+		Connection connection = null;
+		//SQL语句对象
+		PreparedStatement ps = null;
 		
+		try {
+			//1、加载驱动
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//2、获取连接对象
+			connection = DriverManager.getConnection("jdbc:mysql:///jdbcdemo", "root", "admin");
+			//3、获取执行SQL语句对象
+			ps = connection.prepareStatement(sql);
+			ps.setLong(1, id);
+			//4、执行SQL
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (connection != null) {
+						connection.close();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	@Override
 	public void update(RevanStudent student) {
+
+		String sql = "UPDATE t_student SET name = ?, age = ? WHERE id = ?";
+		//贾琏欲执事
+		//获取连接对象
+		Connection connection = null;
+		//SQL语句对象
+		PreparedStatement ps = null;
 		
+		try {
+			//1、加载驱动
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//2、获取连接对象
+			connection = DriverManager.getConnection("jdbc:mysql:///jdbcdemo", "root", "admin");
+			
+			if (get(student.getId()) != null) {
+				//3、获取执行SQL语句对象
+				ps = connection.prepareStatement(sql);
+				ps.setString(1, student.getName());
+				ps.setInt(2, student.getAge());
+				ps.setLong(3, student.getId());
+				//4、执行SQL
+				ps.executeUpdate();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (connection != null) {
+						connection.close();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	
 	}
 
 	@Override
 	public RevanStudent get(Long id) {
+		String sql = "SELECT * FROM t_student WHERE id = ?";
+		//贾琏欲执事
+		//获取连接对象
+		Connection connection = null;
+		//SQL语句对象
+		PreparedStatement ps = null;
+		//结果集
+		ResultSet resultSet = null; 
+		
+		try {
+			//1、加载驱动
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//2、获取连接对象
+			connection = DriverManager.getConnection("jdbc:mysql:///jdbcdemo", "root", "admin");
+			//3、获取执行SQL语句对象
+			ps = connection.prepareStatement(sql);
+			ps.setLong(1, id);
+			//4、执行SQL
+			resultSet = ps.executeQuery();
+			if (resultSet.next()) {
+				RevanStudent student = new RevanStudent();
+				student.setId(resultSet.getLong("id"));
+				student.setName(resultSet.getString("name"));
+				student.setAge(resultSet.getInt("age"));
+				return student;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (ps != null) {
+						ps.close();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				finally {
+					try {
+						if (connection != null) {
+							connection.close();
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
 		return null;
 	}
 
+	/**
+	 *
+	 */
 	@Override
 	public List<RevanStudent> getList() {
-		return null;
+		List<RevanStudent> list = new ArrayList<RevanStudent>();
+
+		String sql = "SELECT * FROM t_student";
+		//贾琏欲执事
+		//获取连接对象
+		Connection connection = null;
+		//SQL语句对象
+		PreparedStatement ps = null;
+		//结果集
+		ResultSet resultSet = null; 
+		
+		try {
+			//1、加载驱动
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//2、获取连接对象
+			connection = DriverManager.getConnection("jdbc:mysql:///jdbcdemo", "root", "admin");
+			//3、获取执行SQL语句对象
+			ps = connection.prepareStatement(sql);
+			//4、执行SQL
+			resultSet = ps.executeQuery();
+			while (resultSet.next()) {
+				RevanStudent student = new RevanStudent();
+				student.setId(resultSet.getLong("id"));
+				student.setName(resultSet.getString("name"));
+				student.setAge(resultSet.getInt("age"));
+				list.add(student);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (ps != null) {
+						ps.close();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				finally {
+					try {
+						if (connection != null) {
+							connection.close();
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return list;
 	}
 
 }
--- src/com/revanwang/test/RevanStudentTest.java
+++ src/com/revanwang/test/RevanStudentTest.java
@@ -3,6 +3,16 @@
  */
 package com.revanwang.test;
 
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.revanwang.dao.IRevanStudentDAO;
+import com.revanwang.dao.impl.RevanStudentDAOImpl;
+import com.revanwang.domain.RevanStudent;
+
 /**
  * @Desc 	
  * @author Revan Wang
@@ -11,4 +21,42 @@ package com.revanwang.test;
  */
 public class RevanStudentTest {
 
+	IRevanStudentDAO studentDAO = new RevanStudentDAOImpl();
+	
+	@Test
+	public void testSave() {
+		RevanStudent student = new RevanStudent();
+		student.setName("乔峰");
+		student.setAge(33);
+		studentDAO.sava(student);
+	}
+	
+	@Test
+	public void testDelete() {
+		studentDAO.delete(13L);
+	}
+	
+	@Test
+	public void testUpdate() {
+		RevanStudent student = new RevanStudent();
+		student.setId(14L);
+		student.setAge(50);
+		student.setName("段正淳");
+		studentDAO.update(student);
+	}
+	
+	@Test
+	public void testGet() {
+		RevanStudent student = studentDAO.get(13L);
+		System.out.println(student.toString());
+	}
+	
+	@Test
+	public void testGetList() {
+		List<RevanStudent> list = studentDAO.getList();
+		Iterator<RevanStudent> it= list.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next().toString());
+		}
+	}
 }
